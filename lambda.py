6acf7fe583051e67b9ee2dc4f9183a26a45fe86b
Michał Woś <mw792829570@gmail.com>
2016-04-09 23:40:03 +0200

    working

--- lambda.py
+++ lambda.py
@@ -9,6 +9,12 @@ http://amzn.to/1LGWsLG
 
 from __future__ import print_function
 import logging
+import urllib2
+import json
+
+
+host = 'http://89.72.80.154:8899'
+
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -68,10 +74,8 @@ def on_intent(intent_request, session):
     # Dispatch to your skill's intent handlers
     if intent_name == "AddProductIntent":
         return add_product(intent, session)
-    elif intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+    #elif intent_name == "WhatsMyColorIntent":
+    #    return get_color_from_session(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -99,13 +103,11 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "What would you like to put to your online bucket"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Please tell me what you would like add to your shopping list by saying for example, " \
+                    "add red wine."
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -113,40 +115,12 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+    speech_output = "Thank you for using Alexa online bucket. You can check online comparison of your products." \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
 
 
 def add_product(intent, session):
@@ -156,8 +130,26 @@ def add_product(intent, session):
     if 'Product' in intent['slots'] and intent['slots']['Product']['value'] is not None:
         product_name = intent['slots']['Product']['value']
         print("Receive product: " + product_name)
-        reprompt_text = "I've just added " + product_name + "to your shop list"
-        speech_output = "Sure! " + product_name + " aded"
+        
+        path = '/resolve/%s'
+        url = host + path % urllib2.quote(product_name)
+        print("Connecting to " + url)
+        resp = urllib2.urlopen(url).read()
+        parsed = json.loads(resp)
+        print("Response status" + parsed['status'])
+        
+        if not 'status' in parsed:
+            reprompt_text = "You din't paid a bill. Server is turned off."
+            speech_output = reprompt_text            
+        elif parsed['status'] == 'OK':
+            reprompt_text = "I've just added " + product_name + "to your shop list"
+            speech_output = "Sure! " + product_name + " added"
+        else:
+            msg = parsed['msg']
+            speech_output = "I have a problem with adding " + product_name
+            if msg:
+                speech_output = speech_output + ". " + msg
+            reprompt_text = "Sorry, problem occured. Try one more time."
     else:
         print("Unspecified product name")
         speech_output = "I'm not sure what product I should add to your shop list. " \
@@ -173,11 +165,6 @@ def add_product(intent, session):
     # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
         intent['name'], speech_output, reprompt_text, should_end_session))
-
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
 
 
 def get_color_from_session(intent, session):
